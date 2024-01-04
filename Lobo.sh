cb3344c60b7769acbdc9960705c42daebb61015a
Angel-OFC-900 <155398692+Angel-OFC-900@users.noreply.github.com>
2024-01-04 14:18:40 -0500

    Actualizar Lobo.sh

--- Lobo.sh
+++ Lobo.sh
@@ -2,7 +2,7 @@
 
 #!/data/data/com.termux/files/usr/bin/bash 
 # Interpretación determinada para la ejecución     
-COMANDOS = "pkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/diegojadibot/SakuraBot-MD\ncd SakuraBot-MD\nyarn install\nnpm install\nnpm start"
+COMANDOS = "pkg install git -y\npkg install nodejs -y\npkg install ffmpeg -y\npkg install imagemagick -y\npkg install -y yarn\ngit clone https://github.com/Angel-OFC-900/LoboBot-MD\ncd LoboBot-MD\nyarn install\nnpm install\nnpm start"
 
 echo -e "\e[38;5;183m
 ─█▀▀█ ───░█ ░█─░█ ░█▀▀▀█ ▀▀█▀▀ ░█▀▀▀ 　 ░█─── ─█▀▀█ 　 ░█▀▀█ ─█▀▀█ ░█▄─░█ ▀▀█▀▀ ─█▀▀█ ░█─── ░█─── ─█▀▀█ 
@@ -118,11 +118,11 @@ echo -e "\e[38;5;183m
 ╚═╝░░╚═╝░░╚═╝░░  ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝\n\e[0m"
 
 echo -e "\033[38;5;188m"
-git clone https://github.com/diegojadibot/SakuraBot-MD.git
+git clone https://github.com/Angel-OFC-900/LoboBot-MD.git
 echo -e "\033[38;5;51m\033[01mLa clonación se ha descargado e instalado correctamente.\n\033[0m"
 
 echo -e "\033[38;5;188m\033[01mCambiando al directorio del repositorio!!\n\033[0m" 
-cd SakuraBot-MD
+cd LoboBot-MD
 
 echo -e "\e[38;5;117m
 █░█ █▀█ █▀▄ ▄▀█ ▀█▀ █▀▀   █▄█ ▄▀█ █▀█ █▄░█
@@ -147,4 +147,4 @@ echo -e "\e[38;38;5;183m
 ░█▄▄█ ░█▄▄▀ ░█▀▀▀ ░█▀▀▀ ░█▀▀▀ ░█▄▄▀ ░█▀▀▀ ░█░█░█ ░█─── ░█─ ░█▄▄█ 
 ░█─── ░█─░█ ░█▄▄▄ ░█─── ░█▄▄▄ ░█─░█ ░█▄▄▄ ░█──▀█ ░█▄▄█ ▄█▄ ░█─░█\n\e[0m"
     
-echo -e "\033[38;5;188m\033[01m🚀 Iniciando SakuraBot-MD!!\n\033[0m"
+echo -e "\033[38;5;188m\033[01m🚀 Iniciando LoboBot-MD!!\n\033[0m"
