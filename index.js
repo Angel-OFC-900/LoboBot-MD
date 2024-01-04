e6ae18759cb4bb620a1e383f4baaaf0f97c41fa4
Angel-OFC-900 <155398692+Angel-OFC-900@users.noreply.github.com>
2024-01-04 15:03:02 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Sakura-MD\nWhatsApp Bot MD', {
+say('LoboBot-MD\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Diego`, {
+say(`Bot creado por Angel`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
